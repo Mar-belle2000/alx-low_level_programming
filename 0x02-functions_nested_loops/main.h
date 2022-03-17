e818379f7fa96562833c58255f49f995568e4927
mary <maryabbah2000@gmail.com>
2022-03-17 11:41:50 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,20 @@
-#include <unistd.h>
-#include "main.h"
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-/**
- * main - Check description
- * Description: It prints the word Holberton, followed by a new line.
- * Return: 0.
- */
-int main(void)
-{char word[8] = "_putchar";
-int i;
-
-for (i = 0; i < 8; i++)
-_putchar(word[i]);
-_putchar('\n');
-return (0);
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+void print_to_98(int n);
+void print_times_table(int n);
+#endif
