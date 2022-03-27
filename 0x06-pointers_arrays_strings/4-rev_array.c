f461031edf255b4d94740b62da27ea7f034addd2
mary <maryabbah2000@gmail.com>
2022-03-26 23:05:28 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *reverse_array - compares two strings
+~ * @a: integer array
+ * @n: number of elements in the array
+ *
+ * Return: 0
+ */
+void reverse_array(int *a, int n)
+{
+	int *p, i, aux, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		aux = a[k];
+		a[k] = *p;
+		*p = aux;
+		p--;
+	}
+}
